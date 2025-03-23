c00f68d2c920eaefc305dd772f22b7fa09a76bc9
John-Kanello <ioanniskanello@gmail.com>
2025-03-23 15:01:31 +0200

    Removed TaskManagementUser field from TaskDetailsRequestDto and converted it to a record class

--- src/taskmanagement/model/dto/request/TaskDetailsRequestDto.java
+++ src/taskmanagement/model/dto/request/TaskDetailsRequestDto.java
@@ -1,18 +1,8 @@
 package taskmanagement.model.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
-import taskmanagement.model.entity.TaskManagementUser;
 
-@NoArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode
-@ToString
-public class TaskDetailsRequestDto {
-    @NotBlank
-    private String title;
-    @NotBlank
-    private String description;
-    private TaskManagementUser author;
-}
+public record TaskDetailsRequestDto(
+        @NotBlank String title,
+        @NotBlank String description
+) {}
